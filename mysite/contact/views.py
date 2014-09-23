282f252d769fa33ef4021f085bc3c9e1438605b3
rohit vernekar <rohit@RohitCentos.(none)>
2014-09-24 01:47:31 +0530

    Configured settings for google mailserver & changed logic in contact/views.py & updated .gitignore with *.pyc

--- mysite/contact/views.py
+++ mysite/contact/views.py
@@ -2,8 +2,8 @@ from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+
 def contact(request):
-    import pdb;pdb.set_trace()
     errors = []
     if request.method == 'POST':
         if not request.POST.get('subject', ''):
@@ -11,17 +11,20 @@ def contact(request):
         if not request.POST.get('message', ''):
             errors.append('Enter a message.')
         if request.POST.get('email') and '@' not in request.POST['email']:
-            errors.append('Enter a valid e-mail address')
+            errors.append('Enter a valid e-mail address.')
         if not errors:
             send_mail(
-                request.POST('subject'),
-                request.POST('message'),
-                request.POST.get('email','grohit@zeomega.com'),['rohitvernekar007@gmail.com'],
+                request.POST['subject'],
+                request.POST['message'],
+                'rohitvernekar007@gmail.com',
+                ['grohit@zeomega.com']
+                
             )
             return HttpResponseRedirect('/contact/thanks/')
     return render(request, 'contact_form.html', {
-            'errors': errors,
-            'subject': request.POST.get('subject', ''),
-            'message': request.POST.get('message', ''),
-            'email': request.POST.get('email', ''),
+        'errors': errors,
+        'subject': request.POST.get('subject', ''),
+        'message': request.POST.get('message', ''),
+        'email': request.POST.get('email', ''),
     })
+
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -156,3 +156,11 @@ LOGGING = {
         },
     }
 }
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587 
+EMAIL_HOST_USER ='username@gmail.com'
+EMAIL_HOST_PASSWORD = '******'
+
+
